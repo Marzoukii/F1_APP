3fa317f3977e1287bb752995a9639a850935b981
ChadiMarzouki <chadi.m@pixelstrade.com>
2022-06-17 23:36:28 +0200

    Add the necessary controls on the fields

--- app/src/main/java/com/example/f1_app/LoginFragment.kt
+++ app/src/main/java/com/example/f1_app/LoginFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.example.f1_app.databinding.FragmentLoginBinding
 import kotlinx.android.synthetic.main.fragment_login.*
 
@@ -17,6 +16,7 @@ class LoginFragment : Fragment() {
 
     private  var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private  var res = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,25 +30,60 @@ class LoginFragment : Fragment() {
 
 
         binding.btnLogin.setOnClickListener {
-            if (binding.txtfirstname.text.isEmpty() ){
-                binding.txtfirstname.error="Champ obligatoire!"
 
-            } else if (binding.txtlastname.text.isEmpty()){
-                binding.txtlastname.error="Champ obligatoire!"
-            } else if (!binding.txtfirstname.text.isEmpty()) {
-                //  val intent = Intent(this, MainActivity::class.java)
+                   if(verifUserInput()) {
 
-                val bundle = bundleOf("firstname" to txtfirstname.text.toString(),"lastname" to txtlastname.text.toString())
 
-                NavHostFragment.findNavController(this)
-                    .navigate(R.id.action_loginFragment_to_dataFragment,bundle)
-                //  intent.putExtra("firstname", txtfirstname.text.toString())
-                //  intent.putExtra("lastname", txtlastname.text.toString())
-                //  startActivity(intent)
+                       //  val intent = Intent(this, MainActivity::class.java)
 
+                       val bundle = bundleOf(
+                           "firstname" to txtfirstname.text.toString(),
+                           "lastname" to txtlastname.text.toString()
+                       )
+
+                       NavHostFragment.findNavController(this)
+                           .navigate(R.id.action_loginFragment_to_dataFragment, bundle)
+                       //  intent.putExtra("firstname", txtfirstname.text.toString())
+                       //  intent.putExtra("lastname", txtlastname.text.toString())
+                       //  startActivity(intent)
+                   }
             }
         }
+    private fun verifUserInput():Boolean{
 
+        if (binding.txtfirstname.text.isEmpty() ) {
+
+            binding.txtfirstname.error = "Please specify your first name "
+            return false
+        }
+        if (binding.txtlastname.text.isEmpty()) {
+
+            binding.txtlastname.error = "Please specify your last name"
+            return false
+        }
+        if(binding.txtfirstname.text.matches("^(?=.*[_.()$&@*+#£?!;-]).*$".toRegex())){
+
+            binding.txtfirstname.error = "Special characters are forbidden"
+            return false
+        }
+
+       if(binding.txtlastname.text.matches("^(?=.*[_.()$&@*+#£?!;-]).*$".toRegex())){
+
+            binding.txtlastname.error = "Special characters are forbidden"
+            return false
+        }
+        if(binding.txtlastname.text.matches("(.*[0-9].*)".toRegex())){
+
+            binding.txtlastname.error = "Numbers are forbidden"
+            return false
+        }
+        if(binding.txtfirstname.text.matches("(.*[0-9].*)".toRegex())){
+
+            binding.txtfirstname.error = "Numbers are forbidden"
+            return false
+        }
+
+        return true
+    }
     }
 
-}
