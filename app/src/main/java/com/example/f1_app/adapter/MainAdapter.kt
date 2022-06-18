1c9070fe7f6c4252a3ba8cb36f5663b145b50efe
ChadiMarzouki <chadi.m@pixelstrade.com>
2022-06-18 16:05:37 +0200

    Update (ViewBinding Adapter)

--- app/src/main/java/com/example/f1_app/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/f1_app/adapter/MainAdapter.kt
@@ -7,23 +7,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.f1_app.model.Pilote
 import com.example.f1_app.R
+import com.example.f1_app.databinding.PostItemBinding
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.post_item.*
 import kotlinx.android.synthetic.main.post_item.view.*
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class MainAdapter(var items: List<Pilote>): RecyclerView.Adapter<MainAdapter.PostHolder>() {
     override fun getItemCount() = items.size
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PostHolder(
-        LayoutInflater.from(parent.context).inflate(
-        R.layout.post_item, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = PostItemBinding.inflate(inflater, parent, false)
+        return PostHolder(binding)
+    }
     override fun onBindViewHolder(holder: PostHolder, position: Int) { holder.bind(items[position]) }
 
-    inner class PostHolder(override val containerView: View): RecyclerView.ViewHolder(containerView),
-        LayoutContainer {
+    inner class PostHolder(private val postItemBinding: PostItemBinding) :
+        RecyclerView.ViewHolder(postItemBinding.root) {
         fun bind(item: Pilote) {
-            textID.text = item.id.toString()
-            title_text.text = item.nomPilote
-            description.text = item.description
-            Glide.with(containerView.context).load(item.photo).into(containerView.image)       }
+            postItemBinding.textID.text = item.id.toString()
+            postItemBinding.titleText.text = item.nomPilote
+            postItemBinding.description.text = item.description
+            Glide.with(postItemBinding.root).load(item.photo).into(postItemBinding.image)       }
     }
 }
