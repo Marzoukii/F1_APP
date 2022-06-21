a1443facd33bdb2d948f6a43897de290639cb1aa
ChadiMarzouki <chadi.m@pixelstrade.com>
2022-06-21 12:27:30 +0200

    ViewModel (Live Data)

--- app/src/main/java/com/example/f1_app/DataFragment.kt
+++ app/src/main/java/com/example/f1_app/DataFragment.kt
@@ -1,24 +1,24 @@
 package com.example.f1_app
 
-import com.example.f1_app.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.f1_app.adapter.MainAdapter
 import com.example.f1_app.databinding.FragmentDataBinding
-import com.example.f1_app.model.Pilote
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.example.f1_app.viewModel.PiloteViewModel
 
 
 class DataFragment : Fragment() {
 
     private  var _binding: FragmentDataBinding? = null
     private val binding get() = _binding!!
-    val listPilote = mutableListOf(
+   /* val listPilote = mutableListOf(
         Pilote(
             1,
             R.drawable.f1,
@@ -79,27 +79,50 @@ class DataFragment : Fragment() {
             "Fernando Alonso",
             "Alpine Renault"
         ),
-    )
+    )*/
 
-
+private lateinit var piloteViewModel: PiloteViewModel
+private lateinit var linearLayoutManager: LinearLayoutManager
+private val mainAdapter by lazy {
+   MainAdapter()
+}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         _binding = FragmentDataBinding.inflate(inflater, container, false)
+
+
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        piloteViewModel=ViewModelProvider(this).get(PiloteViewModel::class.java)
+        linearLayoutManager= LinearLayoutManager(this.context, RecyclerView.VERTICAL, false);
+
+        piloteViewModel=ViewModelProvider(this).get(PiloteViewModel::class.java)
+
         var first = arguments?.getString("firstname")
         var last = arguments?.getString("lastname")
 
+        binding.recy.apply {
+layoutManager=linearLayoutManager
+            adapter=mainAdapter
+        }
+        piloteViewModel.getPilotes()
+
+        piloteViewModel.pilotes.observe(viewLifecycleOwner, Observer {
+            mainAdapter.submitList(it)
+        })
         binding.nom.text =first.toString()
         binding.Prenom.text=last.toString()
-        val adapter = MainAdapter(listOf())
+       /* val adapter = MainAdapter(listOf())
 
         binding.recy.adapter = adapter
         binding.progress.visibility = View.VISIBLE
@@ -109,15 +132,11 @@ class DataFragment : Fragment() {
             binding.progress.visibility = View.GONE
 
             adapter.items = listPilote
-            adapter.notifyDataSetChanged()
+            adapter.notifyDataSetChanged()*/
 
 
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 
-}
+
--- app/src/main/java/com/example/f1_app/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/f1_app/adapter/MainAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.f1_app.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.f1_app.model.Pilote
@@ -13,21 +17,41 @@ import kotlinx.android.synthetic.main.post_item.*
 import kotlinx.android.synthetic.main.post_item.view.*
 import kotlinx.coroutines.NonDisposableHandle.parent
 
-class MainAdapter(var items: List<Pilote>): RecyclerView.Adapter<MainAdapter.PostHolder>() {
-    override fun getItemCount() = items.size
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = PostItemBinding.inflate(inflater, parent, false)
-        return PostHolder(binding)
-    }
-    override fun onBindViewHolder(holder: PostHolder, position: Int) { holder.bind(items[position]) }
+class MainAdapter: ListAdapter<Pilote,MainAdapter.PiloteRecyclerViewHolder>(PilotesDiffutilCallbak()) {
 
-    inner class PostHolder(private val postItemBinding: PostItemBinding) :
-        RecyclerView.ViewHolder(postItemBinding.root) {
-        fun bind(item: Pilote) {
-            postItemBinding.textID.text = item.id.toString()
-            postItemBinding.titleText.text = item.nomPilote
-            postItemBinding.description.text = item.description
-            Glide.with(postItemBinding.root).load(item.photo).into(postItemBinding.image)       }
+
+    class PiloteRecyclerViewHolder(val binding: PostItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    class PilotesDiffutilCallbak : DiffUtil.ItemCallback<Pilote>() {
+        override fun areItemsTheSame(oldItem: Pilote, newItem: Pilote): Boolean {
+            return oldItem == newItem
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: Pilote, newItem: Pilote): Boolean {
+            return oldItem === newItem
+        }
+
+    }
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PiloteRecyclerViewHolder {
+        val inflater = ContextCompat.getSystemService(
+            parent.context,
+            LayoutInflater::class.java
+        ) as LayoutInflater
+        val binding = PostItemBinding.inflate(inflater, parent, false)
+        return PiloteRecyclerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PiloteRecyclerViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.textID.text = item.id.toString()
+        holder.binding.titleText.text = item.nomPilote
+        holder.binding.description.text = item.description
+        Glide.with(holder.binding.root).load(item.photo).into(holder.binding.image)
     }
 }
